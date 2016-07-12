08ec801f2fdd21ca148dddd71d0f7b1b06cfa91b
Marc Morera <yuhu@mmoreram.com>
2016-07-12 10:59:40 +0200

    Fixed namespaces

--- RSQueueBundle.php
+++ RSQueueBundle.php
@@ -2,11 +2,25 @@
 
 namespace RSQueueBundle;
 
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+use RSQueueBundle\DependencyInjection\RSQueueExtension;
 
 /**
  * Main RSQueueBundle class
  */
 class RSQueueBundle extends Bundle
 {
+    /**
+     * Returns the bundle's container extension.
+     *
+     * @return ExtensionInterface|null The container extension
+     *
+     * @throws \LogicException
+     */
+    public function getContainerExtension()
+    {
+        return new RSQueueExtension($this);
+    }
 }
